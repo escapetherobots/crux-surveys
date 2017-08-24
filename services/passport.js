9706c87c1bb1447279a265e8f95f9cccc7aa533f
r2z2 <escapetherobots@gmail.com>
2017-08-24 16:45:05 -0600

    update google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -37,7 +37,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true // allow proxy requests with heroku
     },
     (accessToken, refreshToken, profile, done) => {
       // console.log("google access token:::",accessToken);
