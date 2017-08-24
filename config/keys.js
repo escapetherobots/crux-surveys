05f51f91ff33204b0054ef27ef9a8466fe4d2bc2
r2z2 <escapetherobots@gmail.com>
2017-08-24 16:23:26 -0600

    Setup dev/prod env variables

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+// keys.js - determine dev or prod environment
+// node_env on heroku will contain string = production
+if(process.env.NODE_ENV === 'production'){
+  // production environment - return heroku keys
+  module.exports = require('./prod');
+
+} else {
+  // development environment - return dev keys
+  module.exports = require('./dev');
+}
