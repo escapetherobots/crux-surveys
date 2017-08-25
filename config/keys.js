ba90ce8f946f7739e3feebae122974434076f9d2
r2z2 <escapetherobots@gmail.com>
2017-08-24 18:34:24 -0600

    WIP

--- config/keys.js
+++ config/keys.js
@@ -2,10 +2,8 @@
 // node_env on heroku will contain string = production
 if(process.env.NODE_ENV === 'production'){
   // production environment - return heroku keys
-
-  const prodKeys = require('./prod');
-  console.log('production keys loading', prodKeys.googleClientID);
-  module.exports = prodKeys;
+  console.log('prod keys loading');
+  module.exports = require('./prod');
 } else {
   // development environment - return dev keys
   console.log('dev keys loading');
