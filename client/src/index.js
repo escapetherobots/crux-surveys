2af9dec6cde9de06a7e2b44fdad2c7daf32b23c5
r2z2 <escapetherobots@gmail.com>
2017-08-25 10:18:30 -0600

    Setup basic reducers with redux and create store / setup basic react-routing v4

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+import reducers from './reducers'; // from index.js in reducers folder
+
+// CREATE REDUX STORE
+// ============================================
+// 1st arg = callback that returns array of reducers, 2nd arg = object, 3rd arg = method for middleware like thunk
+// const store = createStore( () => [], {}, applyMiddleware());
+const store = createStore( reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
